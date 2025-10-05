598996b50e5ea779ceba6cbb65dd8b59a2b43598
taieb <mtaieb.rafik@gmail.com>
2025-10-06 01:10:46 +0200

    service-logment init

---
+++ logement-service/src/main/java/com/gestiondelocataire/logement_service/LogementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gestiondelocataire.logement_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogementServiceApplication.class, args);
+	}
+
+}
